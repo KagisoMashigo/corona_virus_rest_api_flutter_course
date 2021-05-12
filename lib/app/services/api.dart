e1d69f2d4833f22b1db524410915e70a922471e3
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-05-12 15:34:24 -0400

    add api keys

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -7,4 +7,10 @@ class API {
 
   factory API.sandbox() => API(apiKey: APIKeys.ncovSandboxKey);
   static final String host = 'ncov2019-admin.firebaseapp.com/';
+
+  Uri tokenUri() => Uri(
+        scheme: 'https',
+        host: host,
+        path: 'token',
+      );
 }
