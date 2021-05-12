55b430887163ca4ccdbc68648635be9ba19ec677
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-05-12 15:01:35 -0400

    begin work on API class

---
+++ lib/app/services/api.dart
@@ -0,0 +1,10 @@
+import 'package:corona_virus_rest_api_flutter_course/app/services/api_keys.dart';
+import 'package:flutter/material.dart';
+
+class API {
+  API({@required this.apiKey});
+  final String apiKey;
+
+  factory API.sandbox() => API(apiKey: APIKeys.ncovSandboxKey);
+  static final String host = 'ncov2019-admin.firebaseapp.com/';
+}
