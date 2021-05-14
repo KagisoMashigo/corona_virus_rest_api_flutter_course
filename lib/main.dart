b926e8c28392fd07df8e455b90143445f9c675cd
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2021-05-14 11:19:23 -0400

    update ui to display data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:corona_virus_rest_api_flutter_course/app/services/api.dart';
+import 'package:corona_virus_rest_api_flutter_course/app/services/api_service.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -46,16 +48,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _accessToken = '';
+  int _cases;
+  int _deaths;
 
-  void _incrementCounter() {
+  void _updateAccessToken() async {
+    final apiService = APIService(API.sandbox());
+    final accessToken = await apiService.getAccessToken();
+    final cases = await apiService.getEndpointData(
+      accessToken: accessToken,
+      endpoint: Endpoint.cases,
+    );
+    final deaths = await apiService.getEndpointData(
+      accessToken: accessToken,
+      endpoint: Endpoint.deaths,
+    );
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _cases = cases;
+      _accessToken = accessToken;
+      _deaths = deaths;
     });
   }
 
@@ -97,14 +109,24 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              'Token: $_accessToken',
               style: Theme.of(context).textTheme.headline4,
             ),
+            if (_cases != null)
+              Text(
+                'Cases: $_cases',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            if (_deaths != null)
+              Text(
+                'Deaths: $_deaths',
+                style: Theme.of(context).textTheme.headline4,
+              ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _updateAccessToken,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
